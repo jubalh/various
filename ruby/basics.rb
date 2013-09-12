445758c3150cf1676233582d7fe42ca8a12a47e3
Michael Vetter <g.bluehut@gmail.com>
2013-09-12 15:10:05 +0200

    added old ruby piece

---
+++ ruby/basics.rb
@@ -0,0 +1,18 @@
+#! /usr/bin/env ruby -w
+
+var = "hey"
+tex = "you!"
+
+text = var + tex
+puts text
+
+class PersonalChef
+  def make_toast(color)
+      puts "Making your toast #{color}!"
+  end
+  def make_eggs(number)
+    puts "Made #{number} eggs!"
+    return self
+  end
+end
+
